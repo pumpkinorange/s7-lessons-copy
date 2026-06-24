1cb7a670ec4d9a52d9eb742e07270e32873b1a00
pumpkinorange <165155445+pumpkinorange@users.noreply.github.com>
2026-06-24 13:10:44 +0300

    4/1

---
+++ Тема 4. PySpark для инженера данных/4. Создание DataFrame и базовые операции/Задание 1/task1.py
@@ -0,0 +1,11 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .master("local") \
+    .appName("Creating DataFrames") \
+    .getOrCreate()
+
+data = [("Max", 55), ("Yan", 53), ("Dmitry", 54), ("Ann", 25)]
+
+df = spark.createDataFrame(data, schema=["Name", "Age"])
+df.printSchema()
