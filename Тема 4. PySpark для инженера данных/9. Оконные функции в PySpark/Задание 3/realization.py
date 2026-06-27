e12ee23ec8dcba7276ef11c904024e9961af4124
Danila Ivanov <danila.ivanov@aboba.local>
2026-06-27 15:34:55 +0300

    4/9/3

--- Тема 4. PySpark для инженера данных/9. Оконные функции в PySpark/Задание 3/realization.py
+++ Тема 4. PySpark для инженера данных/9. Оконные функции в PySpark/Задание 3/realization.py
@@ -1,20 +1,20 @@
-    import pyspark
-    from pyspark.sql import SparkSession
-    from pyspark.sql.window  ...
-    import pyspark.sql.functions ....
-    
-    spark = SparkSession.builder \
-                        .master("yarn") \
-                        .appName("Learning DataFrames") \
-                        .getOrCreate()
-    
-    events = spark.read.json("/user/master/data/events/date=2022-05-01")
-    
-    window = Window().partitionBy('event. ...').orderBy('...')
-    
-    dfWithLag = events.withColumn("lag_7",F.lag("event. ... ").over(window))
-    
-    dfWithLag.select(...) \
-    .filter(dfWithLag.lag_7. ...) \
-    .orderBy(...) \
-    .show(10, False) 
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.window import Window
+import pyspark.sql.functions as F
+
+spark = SparkSession.builder \
+                    .master("yarn") \
+                    .appName("Learning DataFrames") \
+                    .getOrCreate()
+
+events = spark.read.json("/user/master/data/events/date=2022-05-01")
+
+window = Window().partitionBy('event.message_from').orderBy('event.message_ts')
+
+dfWithLag = events.withColumn("lag_7", F.lag("event.message_to", 7).over(window))
+
+dfWithLag.select(F.col('event.message_from'), F.col('lag_7')) \
+.filter(dfWithLag.lag_7.isNotNull()) \
+.orderBy(F.desc('event.message_from')) \
+.show(10, False)
