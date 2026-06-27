7af41c78f4501ad9f20d07ec855da4ecc4f1867a
Danila Ivanov <danila.ivanov@aboba.local>
2026-06-27 18:15:33 +0300

    4/12/1

---
+++ Тема 4. PySpark для инженера данных/12. Автоматизация джобы/Задание 1/dag.py
@@ -0,0 +1,34 @@
+import os
+import datetime as dt
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+os.environ['HADOOP_CONF_DIR'] = '/etc/hadoop/conf'
+os.environ['YARN_CONF_DIR'] = '/etc/hadoop/conf'
+os.environ['JAVA_HOME'] = '/usr'
+os.environ['SPARK_HOME'] = '/usr/lib/spark'
+os.environ['PYTHONPATH'] = '/usr/local/lib/python3.8'
+
+default_args = {
+    'owner': 's24268544',
+    'start_date': dt.datetime(2022, 5, 1),
+    'retries': 1,
+    'retry_delay': dt.timedelta(minutes=5),
+}
+
+with DAG(
+    'partition_events',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False,
+) as dag:
+
+    partitioning = BashOperator(
+        task_id='partitioning',
+        bash_command=(
+            'spark-submit --master yarn --deploy-mode cluster '
+            '/lessons/partition.py 2022-05-31 '
+            '/user/master/data/events /user/s24268544/data/events'
+        ),
+    )
