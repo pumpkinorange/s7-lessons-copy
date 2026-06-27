7a0872d530a9352c7b16c2ea245250d347d0bb38
Danila Ivanov <danila.ivanov@aboba.local>
2026-06-27 15:30:28 +0300

    4/9/2

--- Тема 4. PySpark для инженера данных/9. Оконные функции в PySpark/Задание 2/precode.py
+++ Тема 4. PySpark для инженера данных/9. Оконные функции в PySpark/Задание 2/precode.py
@@ -6,3 +6,6 @@ spark = SparkSession.builder \
                     .appName("Learning DataFrames") \
                     .getOrCreate()
 events = spark.read.json("/user/master/data/events/date=2022-05-01")
+
+# напишите ваш код ниже
+events.printSchema()
