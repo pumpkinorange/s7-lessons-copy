b72032369013fb6e70ff7920d4d77775354e131c
Danila Ivanov <danila.ivanov@aboba.local>
2026-06-30 20:04:24 +0300

    5/2/2

---
+++ Тема 5. Подготовка данных и наполнение Data Lake/2. Задача 1. обновление справочника тегов/Задание 2/realization.py
@@ -0,0 +1,39 @@
+from datetime import datetime, timedelta
+
+import pyspark.sql.functions as F
+from pyspark.sql import SparkSession
+
+
+def input_paths(date, depth):
+    dt = datetime.strptime(date, '%Y-%m-%d')
+    return [
+        f"/user/s24268544/data/events/"
+        f"date={(dt - timedelta(days=offset)).strftime('%Y-%m-%d')}/"
+        f"event_type=message"
+        for offset in range(depth)
+    ]
+
+
+spark = SparkSession.builder \
+    .master("yarn") \
+    .appName("candidates_d7_pyspark") \
+    .getOrCreate()
+
+# сообщения за 7 дней, начиная с 2022-05-31
+paths = input_paths('2022-05-31', 7)
+messages = spark.read.parquet(*paths)
+
+# общедоступные (проверенные) теги
+verified_tags = spark.read.parquet("/user/master/data/snapshots/tags_verified/actual")
+
+candidates = messages \
+    .where("event.message_channel_to is not null") \
+    .selectExpr("event.message_from as user", "explode(event.tags) as tag") \
+    .groupBy("tag") \
+    .agg(F.countDistinct("user").alias("suggested_count")) \
+    .where("suggested_count >= 100") \
+    .join(verified_tags, "tag", "left_anti")
+
+candidates.write \
+    .mode("overwrite") \
+    .parquet("/user/s24268544/data/analytics/candidates_d7_pyspark")
